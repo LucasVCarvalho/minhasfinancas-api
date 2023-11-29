bf5f896c1b1bf5c0ebf43c43e9f8b354e2667fba
Lucas Carvalho <lukasvianacarvalho@gmail.com>
2023-11-29 15:23:21 -0300

    Commit Inicial

---
+++ src/main/java/com/lvcarvalho/minhasfinancas/model/enums/TipoLancamento.java
@@ -0,0 +1,8 @@
+package com.lvcarvalho.minhasfinancas.model.enums;
+
+public enum TipoLancamento {
+	
+	RECEITA,
+	DESPESA
+
+}
---
+++ src/main/java/com/lvcarvalho/minhasfinancas/model/repository/LancamentoRepository.java
@@ -0,0 +1,9 @@
+package com.lvcarvalho.minhasfinancas.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lvcarvalho.minhasfinancas.model.entity.Lancamento;
+
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
+
+}
