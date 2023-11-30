50af106073cb29fd91b663fd623e79c39bc1e758
Lucas Carvalho <lukasvianacarvalho@gmail.com>
2023-11-30 17:37:51 -0300

    concluido o controller de lancamentos

--- src/main/java/com/lvcarvalho/minhasfinancas/api/resource/UsuarioResource.java
+++ src/main/java/com/lvcarvalho/minhasfinancas/api/resource/UsuarioResource.java
@@ -1,8 +1,12 @@
 package com.lvcarvalho.minhasfinancas.api.resource;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,18 +16,24 @@ import com.lvcarvalho.minhasfinancas.api.dto.UsuarioDTO;
 import com.lvcarvalho.minhasfinancas.exception.ErroAutenticacao;
 import com.lvcarvalho.minhasfinancas.exception.RegraNegocioException;
 import com.lvcarvalho.minhasfinancas.model.entity.Usuario;
+import com.lvcarvalho.minhasfinancas.service.LancamentoService;
 import com.lvcarvalho.minhasfinancas.service.UsuarioService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
 //Controller
 public class UsuarioResource {
 
-	private UsuarioService service;
+	private final UsuarioService service;
+	private final LancamentoService lancamentoService;
 	
+	/*
 	public UsuarioResource(UsuarioService service) {
 		this.service = service;
-	}
+	}*/
 	
 	@PostMapping("/autenticar")
 	public ResponseEntity autenticar(@RequestBody UsuarioDTO dto) {
@@ -50,6 +60,18 @@ public class UsuarioResource {
 		} catch (RegraNegocioException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
+	}
+	
+	@GetMapping("{id}/saldo")
+	public ResponseEntity obterSaldo(@PathVariable Long id) {
+		Optional<Usuario> usuario = service.obterPorId(id);
+		
+		if(!usuario.isPresent()) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+		
+		BigDecimal saldo = lancamentoService.obterSaldoPorUsuario(id);
+		return ResponseEntity.ok(saldo);
 	}
 	
 }
